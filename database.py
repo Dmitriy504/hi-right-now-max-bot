e0e58b0953df8da3b4e2b248452d76fdd3237d44
Dmitriy504 <9252018341@ya.ru>
2026-07-22 16:27:39 +0300

    Implement user table and management functions
    Added user management functions and created users table.

--- database.py
+++ database.py
@@ -3,7 +3,44 @@ import psycopg2
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL is empty!")
+
 print("DATABASE_URL =", DATABASE_URL)
 
 conn = psycopg2.connect(DATABASE_URL)
 conn.autocommit = True
+
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id BIGINT PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
+)
+""")
+
+
+def user_exists(user_id):
+    cur.execute(
+        "SELECT 1 FROM users WHERE user_id=%s",
+        (user_id,)
+    )
+    return cur.fetchone() is not None
+
+
+def add_user(user_id, first_name="", last_name=""):
+    cur.execute(
+        """
+        INSERT INTO users(user_id, first_name, last_name)
+        VALUES (%s,%s,%s)
+        ON CONFLICT (user_id) DO NOTHING
+        """,
+        (
+            user_id,
+            first_name,
+            last_name
+        )
+    )

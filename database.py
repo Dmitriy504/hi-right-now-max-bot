e023c4999931e4df01dc917400481409190f5063
Dmitriy504 <9252018341@ya.ru>
2026-07-22 11:25:00 +0300

    Migrate database from SQLite to PostgreSQL

--- database.py
+++ database.py
@@ -1,33 +1,37 @@
-import sqlite3
+import os
+import psycopg2
 
-conn = sqlite3.connect("bot.db", check_same_thread=False)
-cursor = conn.cursor()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-cursor.execute("""
+conn = psycopg2.connect(DATABASE_URL)
+conn.autocommit = True
+
+cur = conn.cursor()
+
+cur.execute("""
 CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY,
+    user_id BIGINT PRIMARY KEY,
     first_name TEXT,
-    last_name TEXT
+    last_name TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
 )
 """)
 
-conn.commit()
 
-
-def user_exists(user_id: int) -> bool:
-    cursor.execute(
-        "SELECT 1 FROM users WHERE user_id=?",
+def user_exists(user_id):
+    cur.execute(
+        "SELECT 1 FROM users WHERE user_id=%s",
         (user_id,)
     )
-    return cursor.fetchone() is not None
+    return cur.fetchone() is not None
 
 
-def add_user(user_id: int, first_name: str, last_name: str):
-    cursor.execute(
+def add_user(user_id, first_name, last_name):
+    cur.execute(
         """
-        INSERT OR IGNORE INTO users
-        (user_id, first_name, last_name)
-        VALUES (?, ?, ?)
+        INSERT INTO users(user_id, first_name, last_name)
+        VALUES (%s,%s,%s)
+        ON CONFLICT (user_id) DO NOTHING
         """,
         (
             user_id,
@@ -35,5 +39,3 @@ def add_user(user_id: int, first_name: str, last_name: str):
             last_name
         )
     )
-
-    conn.commit()
